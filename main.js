bfef66c153bc1f6804280ab82d8db4df632bf6d0
unknown <megajpc@gmail.com>
2013-12-21 20:08:41 +0000

    Data Read/Write Fix

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ $(document).ready(function()
 	.attr('type', 'button')
 	.on('click',function() {
 
-		if(flag==0){
-			bootbox.confirm("<div style='text-align:center'><h3 class='title'><b>Insert your code</b></h3><br><br><input id='code' type='password' class='input form-control'><br><br><button id='num' value='1' type='button' class='buttons-left-middle btn btn-default'> 1 </button><button id='num' value='2' type='button' class='buttons-left-middle btn btn-default'> 2 </button><button id='num' value='3' type='button' class='buttons-right btn btn-default'> 3 </button><br><br><button id='num' value='4' type='button' class='buttons-left-middle btn btn-default'> 4 </button><button id='num' value='5' type='button' class='buttons-left-middle btn btn-default'> 5 </button><button id='num' value='6' type='button' class='buttons-right btn btn-default'> 6 </button><br><br><button id='num' value='7' type='button' class='buttons-left-middle btn btn-default'> 7 </button><button id='num' value='8' type='button' class='buttons-left-middle btn btn-default'> 8 </button><button id='num' value='9' type='button' class='buttons-right btn btn-default'> 9 </button><br><br><button id='num' value='0' type='button' class='buttons-right btn btn-default'> 0 </button></div>", function(result) {
+		if(flag==0)
+		{
+			bootbox.confirm("<div style='text-align:center'>                                                                                                                                                                  <h3 class='title'><b>Insira o código</b></h3>                                                                                                                                                                 <br><br>																																																	  <input id='code' type='password' class='input form-control'>																																				    <br><br>																																																      <button id='num' value='1' type='button' class='buttons-left-middle btn btn-default'> 1 </button>																											    <button id='num' value='2' type='button' class='buttons-left-middle btn btn-default'> 2 </button>																									          <button id='num' value='3' type='button' class='buttons-right btn btn-default'> 3 </button>																													<br><br>																																																	  <button id='num' value='4' type='button' class='buttons-left-middle btn btn-default'> 4 </button>																											    <button id='num' value='5' type='button' class='buttons-left-middle btn btn-default'> 5 </button>																										      <button id='num' value='6' type='button' class='buttons-right btn btn-default'> 6 </button>																													<br><br>																																																      <button id='num' value='7' type='button' class='buttons-left-middle btn btn-default'> 7 </button>																								                <button id='num' value='8' type='button' class='buttons-left-middle btn btn-default'> 8 </button>																						                      <button id='num' value='9' type='button' class='buttons-right btn btn-default'> 9 </button>																												    <br><br>																																																      <button id='num' value='0' type='button' class='buttons-right btn btn-default'> 0 </button>																													</div>", function(result) {
 
 					if(x == $('#code').val())
 					{
@@ -28,7 +29,9 @@ $(document).ready(function()
 					x = $('#code').val();
 					$('#code').val(x+$(this).val());
 				});
-		}else{
+		}
+		else
+		{
 			flag = 0;
 		}
 	});
--- options.js
+++ options.js
@@ -2,46 +2,93 @@ function getData()
 {
 	chrome.storage.local.get(null, function(items) {
 	    var allKeys = Object.keys(items);
-	    console.log(allKeys);
+	    //console.log(allKeys);
+
+	    var table = "<table align='center' class='text table table-striped'>";
+
+		table += "<tr>																																																		  <td style='width: 50px; text-align: center;'><b> # </b></td>																																					    <td style='width: 200px; text-align: center;'><b> E-Mail </b></td>																																				  <td style='width: 100px; text-align: center;'><b> Código </b></td>																																				<td style='width: 100px; text-align: center;'><b> Edição </b></td>																																				  </tr>"
+
+		for (var i = 0; i < allKeys.length; i++) 
+		{
+			var Email = allKeys[i];
+
+			table += "<tr><td style='width: 100px; text-align: center;'> " 																																					  + (i+1) + 																																														            "</td><td style='width: 100px; text-align: center;'>" 																																                          + allKeys[i] + 																																														        "</td><td style='width: 100px; text-align: center;'>" 																																		                  + items[allKeys[i]] + 																																														"</td><td style='width: 100px; text-align: center;'>																																                          <button style='margin-right: 10px;' id='edit' type='button' class='btn btn-default btn-lg'><span class='glyphicon glyphicon-pencil'></span></button>															<button id='remove' type='button' class='btn btn-default btn-lg'><span class='glyphicon glyphicon-remove'></span></button></td></tr>";
+			
+		}
+
+		table += "</table>";
+
+		document.getElementById('table-print').innerHTML = table;
+
+		var bt_remove = document.getElementById ("remove");
+
+		bt_remove.addEventListener("click", function () {removeUser()}, false);
 	});
+	
+}
+
+function setData()
+{
+	chrome.storage.local.set({'megajpc': '123'});
+	chrome.storage.local.set({'ramon': '456'});
+	chrome.storage.local.set({'sandra': '789'});
+	chrome.storage.local.set({'sofia': '0'});
 }
 
 function returnData(Email)
 {
-	chrome.storage.local.get('Email', function (result) {
+	chrome.storage.local.get(Email, function (result) {
 
-		var code = result.Email;
-
-		alert("Email: " + Email + ", Código: " + code);
-
+		console.log(Email, result[Email]);
 	});
 }
 
 function clickHandler(e) 
 {
-  bootbox.confirm("<form role='form'><br><br><div class='title'><h3><b>Instruções</b></h3></div><br><div class='text'>O Email escolhido deve ser <b>obrigatoriamente</b> igual ao Email do Facebook; <br><br>O Código só deve conter números entre <b>0</b> e <b>9</b>;</div><br><br><div class='form-group'><label class='text' for='Email'><b>Email</b></label><br><input type='email' class='form-control' id='Email'></div><br><div class='form-group'><label class='text' for='Password1'><b>Código</b></label><br><input type='password' class='form-control' id='Password1'></div><br><div class='form-group'><label class='text' for='Password2'><b>Confirmar Código</b></label><br><input type='password' class='form-control' id='Password2'></div></form>", function(result) {
+	bootbox.confirm("<form role='form'>																																														  <br><br>																																																		        <div class='title'>																																															          <h3><b>Instruções</b></h3>																																													        </div>																																																		          <br>																																																			        <div class='text'>O Email escolhido deve ser <b>obrigatoriamente</b> igual ao Email do Facebook; <br><br>O Código só deve conter números entre <b>0</b> e <b>9</b>;											          </div>																																																		        <br><br>																																																		      <div class='form-group'>																																														        <label class='text' for='Email'><b>Email</b></label>																																							      <br>																																																		            <input type='email' class='form-control' id='Email'>																																							      </div>																																																		        <br>																																																			      <div class='form-group'>																																														        <label class='text' for='Password1'><b>Código</b></label>																																						      <br>																																																			        <input type='password' class='form-control' id='Password1'>																																					          </div> 																																																		        <br>																																																			      <div class='form-group'>																																														        <label class='text' for='Password2'><b>Confirmar Código</b></label>																																			          <br>																																																			        <input type='password' class='form-control' id='Password2'>																																					          </div>																																																		        </form>", function(result) {
 
-	if($('#Password1').val() == $('#Password2').val())
-	{
-		var Email = $('#Email').val();
-		var code = $('#Password1').val();
+			var obj = {};
+			var email = $('#Email').val();
+			var code = $('#Password1').val();
 
-		chrome.storage.local.set({'Email': code});
+			//Primeiro vamos verificar se já não existe um utilizador registado com o mesmo e-mail
 
-		bootbox.alert("<br><div class='text'><b>Dados guardados com sucesso.</b></div>");
+			chrome.storage.local.get(email, function (result) {
 
-		//returnData(Email);
-		//getData();
-	}
-	else
-	{
-		bootbox.alert("<br><div class='text'><b>Os códigos inseridos não são iguais. <br><br>Tente de novo.</b></div>");
-	}
+			var res = result.Email;
 
-  });
+			if(res)
+			{
+				bootbox.alert("<br><div class='text'><b>Já existe um utilizador com o mesmo Email registado.</b></div>");
+			}
+			else
+			{
+				if($('#Password1').val() == $('#Password2').val())
+				{
+					obj[email] = code;
+
+					chrome.storage.local.set(obj);
+
+					bootbox.alert("<br><div class='text'><b>Dados guardados com sucesso.</b></div>");
+
+					getData();
+				}
+				else
+				{
+					bootbox.alert("<br><div class='text'><b>Os códigos inseridos não são iguais. <br><br>Tente de novo.</b></div>");
+				}
+			}
+		});
+		});
+}
+
+function removeUser()
+{
+	console.log("cheguei");
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('button').addEventListener('click', clickHandler);
+  getData();
 });
 	
