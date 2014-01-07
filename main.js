bf4380dfce8d02eade2f4899c57b26fbeabcdb48
unknown <megajpc@gmail.com>
2014-01-07 17:27:53 +0000

    Pequena reestruturacao do codigo

--- main.js
+++ main.js
@@ -19,6 +19,42 @@ $(document).ready(function()
         	id = c.substring(cookie.length,c.length);
     }
 
+
+    function code_input()
+    {
+    	if(flag == 0)
+		{
+			bootbox.confirm("<div style='text-align:center'>                                                                                                                                                                  <h3 class='title'><b>Insira o código</b></h3>                                                                                                                                                                 <br><br>																																																	  <input id='code' type='password' class='input form-control'>																																				    <br><br>																																																      <button id='num' value='1' type='button' class='buttons-left-middle btn btn-default'> 1 </button>																											    <button id='num' value='2' type='button' class='buttons-left-middle btn btn-default'> 2 </button>																									          <button id='num' value='3' type='button' class='buttons-right btn btn-default'> 3 </button>																													<br><br>																																																	  <button id='num' value='4' type='button' class='buttons-left-middle btn btn-default'> 4 </button>																											    <button id='num' value='5' type='button' class='buttons-left-middle btn btn-default'> 5 </button>																										      <button id='num' value='6' type='button' class='buttons-right btn btn-default'> 6 </button>																													<br><br>																																																      <button id='num' value='7' type='button' class='buttons-left-middle btn btn-default'> 7 </button>																								                <button id='num' value='8' type='button' class='buttons-left-middle btn btn-default'> 8 </button>																						                      <button id='num' value='9' type='button' class='buttons-right btn btn-default'> 9 </button>																												    <br><br>																																																      <button id='num' value='0' type='button' class='buttons-right btn btn-default'> 0 </button>																													</div>", function(result) {
+
+				if(result)
+				{
+					if(_.isEqual(codigo, CryptoJS.SHA3($('#code').val())))
+					{
+						flag = 1;
+
+						$('._42ft.selected')
+							.attr('type', 'submit')
+							.trigger('click');
+					}
+					else
+					{
+						bootbox.alert("<br><div style='text-align:center' class='title'><h3><b>Código errado</b></h3></div>");
+					}
+				}
+			});
+
+			$('button#num').on('click',function(){
+				x = $('#code').val();
+				$('#code').val(x+$(this).val());
+			});
+		}
+		else
+		{
+			flag = 0;
+		}
+    }
+
+
 	//Vamos obter todos os users guardados e comparar o seu id com o id da sessão actual. Se não existir vamos guardar o id através do chrome storage para a possibilidade ser criada uma nova conta com esse id
 
 	chrome.storage.local.get(null, function(items) {
@@ -43,36 +79,8 @@ $(document).ready(function()
 				.attr('type', 'button')
 				.on('click',function() {
 
-				if(flag == 0)
-				{
-					bootbox.confirm("<div style='text-align:center'>                                                                                                                                                                  <h3 class='title'><b>Insira o código</b></h3>                                                                                                                                                                 <br><br>																																																	  <input id='code' type='password' class='input form-control'>																																				    <br><br>																																																      <button id='num' value='1' type='button' class='buttons-left-middle btn btn-default'> 1 </button>																											    <button id='num' value='2' type='button' class='buttons-left-middle btn btn-default'> 2 </button>																									          <button id='num' value='3' type='button' class='buttons-right btn btn-default'> 3 </button>																													<br><br>																																																	  <button id='num' value='4' type='button' class='buttons-left-middle btn btn-default'> 4 </button>																											    <button id='num' value='5' type='button' class='buttons-left-middle btn btn-default'> 5 </button>																										      <button id='num' value='6' type='button' class='buttons-right btn btn-default'> 6 </button>																													<br><br>																																																      <button id='num' value='7' type='button' class='buttons-left-middle btn btn-default'> 7 </button>																								                <button id='num' value='8' type='button' class='buttons-left-middle btn btn-default'> 8 </button>																						                      <button id='num' value='9' type='button' class='buttons-right btn btn-default'> 9 </button>																												    <br><br>																																																      <button id='num' value='0' type='button' class='buttons-right btn btn-default'> 0 </button>																													</div>", function(result) {
+					code_input();
 
-						if(result)
-						{
-							if(_.isEqual(codigo, CryptoJS.SHA3($('#code').val())))
-							{
-								flag = 1;
-
-								$('._42ft.selected')
-									.attr('type', 'submit')
-									.trigger('click');
-							}
-							else
-							{
-								bootbox.alert("<br><div style='text-align:center' class='title'><h3><b>Código errado</b></h3></div>");
-							}
-						}
-					});
-
-					$('button#num').on('click',function(){
-						x = $('#code').val();
-						$('#code').val(x+$(this).val());
-					});
-				}
-				else
-				{
-					flag = 0;
-				}
 			});
 		}
 		else
